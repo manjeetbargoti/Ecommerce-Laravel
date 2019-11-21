77bb64073d3564079ae9b9865a90feba7b6c6597
manjeetbargoti <msb.2905@gmail.com>
2019-11-21 17:59:41 +0400

    Add toaster notification

--- resources/views/layouts/panel/panel_design.blade.php
+++ resources/views/layouts/panel/panel_design.blade.php
@@ -232,8 +232,30 @@
             </td>
         </tr>
         {% } %}
+    </script>
 
-</script>
+    <script>
+    @if(Session::has('message'))
+    var type = "{{ Session::get('alert-type', 'info') }}";
+    switch (type) {
+        case 'info':
+            toastr.info("{{ Session::get('message') }}");
+            break;
+
+        case 'warning':
+            toastr.warning("{{ Session::get('message') }}");
+            break;
+
+        case 'success':
+            toastr.success("{{ Session::get('message') }}");
+            break;
+
+        case 'error':
+            toastr.error("{{ Session::get('message') }}");
+            break;
+    }
+    @endif
+    </script>
 
     <script>
     // Creating Property URL
--- storage/framework/views/334ccc805cb4a63bb52e597ae3ff56ee36f62cf2.php
+++ storage/framework/views/334ccc805cb4a63bb52e597ae3ff56ee36f62cf2.php
@@ -232,8 +232,30 @@
             </td>
         </tr>
         {% } %}
+    </script>
 
-</script>
+    <script>
+    <?php if(Session::has('message')): ?>
+    var type = "<?php echo e(Session::get('alert-type', 'info')); ?>";
+    switch (type) {
+        case 'info':
+            toastr.info("<?php echo e(Session::get('message')); ?>");
+            break;
+
+        case 'warning':
+            toastr.warning("<?php echo e(Session::get('message')); ?>");
+            break;
+
+        case 'success':
+            toastr.success("<?php echo e(Session::get('message')); ?>");
+            break;
+
+        case 'error':
+            toastr.error("<?php echo e(Session::get('message')); ?>");
+            break;
+    }
+    <?php endif; ?>
+    </script>
 
     <script>
     // Creating Property URL
